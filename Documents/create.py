ef75f4d776b67f60d5ff572db1b403bc3d742893
Vivek Nair <vivekaxl@gmail.com>
2018-02-12 14:11:09 -0500

    Creating folders

---
+++ Documents/create.py
@@ -0,0 +1,13 @@
+import os
+
+names = ['timm', 'wfu', 'rahlk', 'vivekaxl', 'jf', 'zhe', 'jack', 'amrit', 'patrick', 'ken', 'junjie']
+
+for name in names:
+	os.system('mkdir ' + name)
+	os.system('mkdir ' + name + '/Journal')
+	os.system('touch ' + name + '/Journal/.keep')
+	os.system('mkdir ' + name + '/Conference')
+	os.system('touch ' + name + '/Conference/.keep')
+
+
+print 'Done'
